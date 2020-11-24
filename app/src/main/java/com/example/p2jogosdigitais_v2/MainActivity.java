450c6d6868ca82a427719f83b544cd3300f84bf4
Rhamer Noro <rhamernoro@gmail.com>
2020-11-23 20:32:26 -0400

    gravando dados no banco

--- app/src/main/java/com/example/p2jogosdigitais_v2/MainActivity.java
+++ app/src/main/java/com/example/p2jogosdigitais_v2/MainActivity.java
@@ -7,11 +7,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    private EditText edit_nome;
-    private EditText edit_idade;
+    EditText edit_nome;
+    EditText edit_idade;
+
+    FirebaseDatabase firebasedatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
         edit_nome = (EditText) findViewById(R.id.edit_nome);
         edit_idade = (EditText) findViewById(R.id.edit_idade);
 
+        iniciarFirebase();
     }
+
+    private void iniciarFirebase() {
+        FirebaseApp.initializeApp(MainActivity. this);
+        firebasedatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebasedatabase.getReference();
+    }
+
 
 
     public void botaoClick(View view){
 
-        Toast.makeText(MainActivity.this, "Olá" + edit_nome.getText() + ", você tem: " + edit_idade.getText() + "anos", Toast.LENGTH_LONG).show();
+        pessoal p = new pessoal();
+        p.setId(UUID.randomUUID().toString());
+        p.setNome(edit_nome.getText().toString());
+
+        String value= edit_idade.getText().toString();
+        int finalValue=Integer.parseInt(value);
+
+        databaseReference.child("Pessoa").child(p.getId()).setValue(p);
+
+       // Toast.makeText(MainActivity.this, "Olá" + edit_nome.getText() + ", você tem: " + edit_idade.getText() + "anos", Toast.LENGTH_LONG).show();
     }
 
 
--- app/src/main/java/com/example/p2jogosdigitais_v2/pessoal.java
+++ app/src/main/java/com/example/p2jogosdigitais_v2/pessoal.java
@@ -2,10 +2,19 @@ package com.example.p2jogosdigitais_v2;
 
 public class pessoal {
 
+    private String id;
     private String nome;
     private Integer idade;
 
     public pessoal() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNome() {

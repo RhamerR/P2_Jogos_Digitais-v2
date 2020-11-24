5e8aacbec68bb19a2d244acf305fe47fbfa178aa
Rhamer Noro <rhamernoro@gmail.com>
2020-11-23 20:01:12 -0400

    classe criada

---
+++ app/src/main/java/com/example/p2jogosdigitais_v2/pessoal.java
@@ -0,0 +1,26 @@
+package com.example.p2jogosdigitais_v2;
+
+public class pessoal {
+
+    private String nome;
+    private Integer idade;
+
+    public pessoal() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
